8d3d0cad47091f7727b837a8abb5e2fa7a14cf25
luckylighter <vknyaz06@gmail.com>
2022-04-24 15:34:54 -0400

    added replay function

--- Marble.py
+++ Marble.py
@@ -1,14 +1,21 @@
+"""
+Veniamin Knyazev
+CS 5001 Spring 2022
+Final Project- Marble Class
+"""
 import turtle
 from Point import Point
 from MarbleData import MarbleData
 from Button import Button
 from mastermind_game_files import make_code, game_logic
 import datetime
+import time
 
 MARBLE_RADIUS = 20
 NOTIFICATION = 8
 COLORS = ["red", "blue", "green", "yellow", "purple", "black"]
-QUIT_BUTTON = "quit_small.gif"
+PLACE = ["st", "nd", "rd", "th", "th", "th", "th", "th", "th", "th"]
+QUIT_BUTTON = "quit_button_small.gif"
 CHECK_BUTTON = "checkbutton.gif"
 X_BUTTON = "xbutton.gif"
 WINNER = "winner.gif"
@@ -16,8 +23,12 @@ QUIT_MSG = "quitmsg.gif"
 LOSE = "Lose.gif"
 FILE_ERROR = "file_error.gif"
 LEADER_ERROR = "leaderboard_error.gif"
+WOOD = "wood.gif"
 
 class Marble:
+    """
+
+    """
     def __init__(self, position, color='white', size=MARBLE_RADIUS):
         self.pen = self.new_pen()
         self.screen = self.new_screen()
@@ -37,39 +48,88 @@ class Marble:
         self.secret_code = make_code()
         self.current_player = self.get_user()
 
+    def new_pen(self):
+        """
 
-    def new_pen(self): 
+        :return:
+        """
         return turtle.Turtle()
 
     def new_screen(self):
+        """
+
+        :return:
+        """
         return turtle.Screen()
 
     def set_color(self, color):
+        """
+
+        :param color:
+        :return:
+        """
         self.color = color
 
     def set_size(self, size):
+        """
+
+        :param size:
+        :return:
+        """
         self.size = size
 
     def get_color(self):
+        """
+
+        :return:
+        """
         return self.color
 
     def get_column(self):
+        """
+
+        :return:
+        """
         return self.current_column
 
     def set_column(self, column):
+        """
+
+        :param column:
+        :return:
+        """
         self.current_column = column
 
     def set_row(self, row):
+        """
+
+        :param row:
+        :return:
+        """
         self.current_row = row
 
     def get_row(self):
+        """
+
+        :return:
+        """
         return self.current_row
 
     def set_position(self, x, y):
+        """
+
+        :param x:
+        :param y:
+        :return:
+        """
         self.position.x = x
         self.position.y = y
 
     def draw(self):
+        """
+
+        :return:
+        """
         # if self.visible and not self.is_empty:
             # return
 
@@ -82,6 +142,10 @@ class Marble:
         self.pen.end_fill()
 
     def draw_empty(self):
+        """
+
+        :return:
+        """
         self.erase()
         self.pen.up()
         self.pen.goto(self.position.x, self.position.y)
@@ -89,15 +153,29 @@ class Marble:
         self.pen.circle(self.size)
         
     def erase(self):
+        """
+
+        :return:
+        """
         self.pen.clear()
 
     def clicked_in_region(self, x, y):
+        """
+
+        :param x:
+        :param y:
+        :return:
+        """
         if abs(x - self.position.x) <= self.size * 2 and \
            abs(y - self.position.y) <= self.size * 2:
             return True
         return False
 
     def add_upper_marbles(self):
+        """
+
+        :return:
+        """
         color = 'white'
         for y in range(350, -250, -65):
             for x in range(-300, -60, 60):
@@ -106,8 +184,19 @@ class Marble:
                 self.marble_data.append(MarbleData(x, y, color))
                 self.set_color(color)
                 self.draw()
+        self.marble_data = [self.marble_data[n:n + 4] for n in
+                            range(0, len(self.marble_data), 4)]
 
     def draw_box(self, width, height, x, y, color='black'):
+        """
+
+        :param width:
+        :param height:
+        :param x:
+        :param y:
+        :param color:
+        :return:
+        """
         RIGHT = 90
         t = self.new_pen()
         t.color(color)
@@ -129,7 +218,12 @@ class Marble:
         t.penup()
 
     def add_lower_marbles(self):
-        self.position = self.marble_position
+        """
+
+        :return:
+        """
+        #self.marble_position = Point(-350, -350)
+        self.position = Point(-350, -350)
         for color in COLORS:
             # record the location of the circle
             # add here ....
@@ -142,6 +236,10 @@ class Marble:
             self.position.x += 60
 
     def add_notification_marbles(self):
+        """
+
+        :return:
+        """
         color = 'white'
         self.set_position(-50, 418)
         for _ in range(10):
@@ -157,8 +255,42 @@ class Marble:
                     self.set_color(color)
                     self.draw()
                     self.position.x += 20
+        self.notification_data = [self.notification_data[n:n + 4] for n in
+                                  range(0, len(self.notification_data), 4)]
+
+    def add_dissaspearing_button(self, image, x, y):
+        """
+
+        :param image:
+        :param x:
+        :param y:
+        :return:
+        """
+        # add image to screen
+        t = self.new_pen()
+        w = self.new_screen()
+        t.hideturtle()
+        t.speed(0)
+        t.penup()
+        w.addshape(image)
+        t.goto(x,y)
+        t.showturtle()
+        t.shape(image)
+        t.showturtle()
+        time.sleep(2)
+        t.hideturtle()
+
+
+
 
     def add_button(self, image, x, y):
+        """
+
+        :param image:
+        :param x:
+        :param y:
+        :return:
+        """
         # add image to screen
         t = self.new_pen()
         w = self.new_screen()
@@ -172,8 +304,13 @@ class Marble:
         t.stamp()
 
     def draw_play_board(self):
+        """
+
+        :return:
+        """
         s = self.new_screen()
         s.setup(900,900)
+        #s.bgpic(WOOD)
         self.draw_box(450, 650, -400, 400)
         self.draw_box(300, 650, 60, 400, 'blue')
         self.draw_box(760, 150, -400, -260)
@@ -184,46 +321,70 @@ class Marble:
         self.add_button(CHECK_BUTTON, 50, -335)
         self.add_button(X_BUTTON, 125, -335)
         self.row_pointer(self.current_row)
-        #self.row_pointer()
 
+    def redraw_play_board(self):
+        """
+
+        :return:
+        """
+        self.input_marble_data.clear()
+        self.marble_data.clear()
+        self.notification_data.clear()
+        self.current_guess = []
+        self.secret_code.clear()
+        self.secret_code = make_code()
+        #self.button_location = []
+        self.add_upper_marbles()
+        self.add_notification_marbles()
+        self.add_lower_marbles()
+        self.current_row = 0
+        self.current_column = 0
+        self.row_pointer(self.current_row)
 
 
     def click_circles(self, x, y):
+        """
+
+        :param x:
+        :param y:
+        :return:
+        """
         #for color, location in self.marble_data:
         self.set_size(MARBLE_RADIUS)
         for input_marble_data in self.input_marble_data:
             if input_marble_data.clicked_in_region(x, y):
                 if input_marble_data.color == 'white' or \
-                        len(self.current_guess) > 3:
-                    break
+                        len(self.current_guess) > 3 or \
+                        self.current_row > 9:
+                    return
                 else:
                     color = input_marble_data.get_color()
-                    x1 = input_marble_data.x
-                    y1 = input_marble_data.y
                     input_marble_data.set_color('white')
-                    self.position.x = x1
-                    self.position.y = y1
+                    self.position.x = input_marble_data.x
+                    self.position.y = input_marble_data.y
                     self.set_color('white')
                     self.draw()
                     row_num = self.get_row()
-                    play_row = [self.marble_data[n:n+4] for n in
-                                  range(0, len(self.marble_data), 4)]
                     column = self.get_column()
-                    if play_row[row_num][column].color == 'white':
+                    if self.marble_data[row_num][column].color == 'white':
                         self.set_color(color)
                         self.current_guess.append(self.get_color())
-                        self.position.x = play_row[row_num][column].x
-                        self.position.y = play_row[row_num][column].y
+                        self.position.x = self.marble_data[row_num][column].x
+                        self.position.y = self.marble_data[row_num][column].y
                         self.draw()
                         self.current_column += 1
-                        break
-                    else:
-                        continue
         print(self.secret_code)
         self.click_buttons(x, y)
-        #self.row_pointer(self.current_row)
+
+
 
     def click_buttons(self, x, y):
+        """
+
+        :param x:
+        :param y:
+        :return:
+        """
         for button_location in self.button_location:
             if button_location.clicked_in_region_button(x, y):
                 if button_location.image == X_BUTTON:
@@ -238,45 +399,44 @@ class Marble:
                     self.quit()
 
     def check_button(self):
+        """
+
+        :return:
+        """
         bulls, cows = game_logic(self.secret_code, self.current_guess)
-        if bulls == 4:
-            self.winner()
         column = 0
         current_row = self.get_row()
-        notification_row = [self.notification_data[n:n + 4] for n in
-                    range(0, len(self.marble_data), 4)]
-        notification_row = notification_row[current_row]
+        self.set_size(NOTIFICATION)
+        notification_row = self.notification_data[current_row]
         for bull in range(bulls):
             self.set_position(notification_row[column].get_position_x(),
                               notification_row[column].get_position_y())
             self.set_color('black')
-            self.set_size(NOTIFICATION)
             self.draw()
             column += 1
         for cow in range(cows):
             self.set_position(notification_row[column].get_position_x(),
                               notification_row[column].get_position_y())
             self.set_color('red')
-            self.set_size(NOTIFICATION)
             self.draw()
             column += 1
+        if bulls == 4:
+            self.winner()
+            return
+
         self.current_row += 1
-        #self.row_delete(self.current_row)
         self.row_pointer(self.current_row)
         self.color_input_marbles()
         if self.current_row > 9:
             self.loser()
 
     def reset_button(self):
+        """
 
-        current_row = self.get_row()
-        play_row = [self.marble_data[n:n + 4] for n in
-                    range(0, len(self.marble_data), 4)]
-        play_row = play_row[current_row]
+        :return:
+        """
 
-
-        # fill play row with empty circles
-        for marbles in play_row:
+        for marbles in self.marble_data[self.current_row]:
             x = marbles.get_position_x()
             y = marbles.get_position_y()
             self.set_position(x,y)
@@ -284,21 +444,54 @@ class Marble:
             self.draw()
 
     def quit(self):
-        self.add_button(QUIT_MSG, 0, 0)
-        turtle.exitonclick()
+        """
+
+        :return:
+        """
+        self.add_dissaspearing_button(QUIT_MSG, 0, 0)
+        turtle.bye()
 
     def winner(self):
+        """
+
+        :return:
+        """
         self.add_score()
-        self.add_button(WINNER, 0, 0)
-        turtle.exitonclick()
+        tr.clear()
+        self.write_score()
+        self.add_dissaspearing_button(WINNER, 0, 0)
+        s = self.new_screen()
+        response = s.textinput("Winner Winner! Chicken Dinner!",
+                               "Play again? type [n] and click ok to quit")
+        if response == 'n':
+            self.quit()
+        else:
+            self.erase()
+            self.redraw_play_board()
+
+
 
     def loser(self):
-        self.add_button(LOSE, 0, 0)
+        """
+
+        :return:
+        """
+        self.add_dissaspearing_button(LOSE, 0, 0)
         s = self.new_screen()
-        s.textinput("Secret Code Was", self.secret_code)
-        turtle.exitonclick()
+        response = s.textinput("Secret Code Was:", str(self.secret_code) + "\n" +
+                               "Play again? type [n] and click ok to quit ")
+        if response == 'n':
+            self.quit()
+        else:
+            self.erase()
+            self.redraw_play_board()
+
 
     def color_input_marbles(self):
+        """
+
+        :return:
+        """
         # re-color lower input circles
         for index in range(len(self.input_marble_data)):
             if self.input_marble_data[index].color == 'white':
@@ -315,7 +508,11 @@ class Marble:
         self.current_column = 0
 
     def row_pointer(self, row):
+        """
 
+        :param row:
+        :return:
+        """
         if row > 9:
             return
         turtle.hideturtle()
@@ -327,12 +524,20 @@ class Marble:
         turtle.shapesize(2, 2, 2)
 
     def get_user(self):
+        """
+
+        :return:
+        """
         s = self.new_screen()
         return s.textinput("Master-Mind Game",
                            "Please enter your name:").strip().capitalize()
 
     def write_errors(self, error):
+        """
 
+        :param error:
+        :return:
+        """
         try:
             with open('error_log.txt', 'a+') as f:
                 date_time = datetime.datetime.now()
@@ -343,8 +548,12 @@ class Marble:
             print(error)
 
     def add_score(self):
+        """
+
+        :return:
+        """
         try:
-            with open('leaderboard1.txt', 'a') as w:
+            with open('leaderboard.txt', 'a') as w:
                 w.write((str(self.current_row + 1)) +
                         " : " + self.current_player.strip().capitalize() + '\n')
 
@@ -355,31 +564,55 @@ class Marble:
 
 
     def write_score(self):
+        """
 
+        :return:
+        """
+        global tr
+        tr = self.new_pen()
+        tr.hideturtle()
+        tr.speed(0)
+        tr.penup()
         try:
-            with open('leaderboard1.txt', 'r') as r:
-                leaders = [line.strip().split(':') for line in r]
+            with open('leaderboard.txt', 'r') as r:
+                leaders = []
+                for line in r:
+                    leaders.append(line.strip().split(':'))
+                #global tr
                 leaders.sort()
-                t = self.new_pen()
-                t.hideturtle()
-                t.speed(0)
-                t.penup()
-                t.goto(75, 300)
-
-                for leader in leaders:
-                    leader = ":".join(leader)
-                    t.write(leader, font=('Verdana', 20, "normal"))
-                    t.right(90)
-                    t.forward(50)
-                    t.left(90)
+                #tr = self.new_pen()
+                #tr.hideturtle()
+                #tr.speed(0)
+                #tr.penup()
+                tr.goto(90, 350)
+                tr.color('red')
+                tr.write("Leaderboard", font=('Verdana', 30, 'normal'))
+                tr.goto(75, 300)
+                for i, leader in enumerate(leaders):
+                    if i > 9:
+                        return
+                    else:
+                        leader = ":".join(leader)
+                        tr.write(str(i + 1) + PLACE[i] + ' place  ' + leader, font=('Verdana', 15, "normal"))
+                        tr.right(90)
+                        tr.forward(50)
+                        tr.left(90)
 
         except IOError as error:
+
+            tr.goto(90, 350)
+            tr.color('red')
+            tr.write("Leaderboard", font=('Verdana', 30, 'normal'))
             self.write_errors(error)
+            self.leader_board_error()
             print(error)
 
     def leader_board_error(self):
-        self.add_button(LEADER_ERROR, 0, 0)
+        """
+
+        :return:
+        """
+        self.add_dissaspearing_button(LEADER_ERROR, 0, 0)
 
     def file_error(self):
-        self.add_button(FILE_ERROR, 0, 0)
-        self.erase()
+        self.add_dissaspearing_button(FILE_ERROR, 0, 0)
