ce88aa764270c9d0d0da49e8eb77d7ce4d9e3d31
luckylighter <vknyaz06@gmail.com>
2022-04-24 15:34:01 -0400

    added replay function

--- mastermind_game.py
+++ mastermind_game.py
@@ -1,14 +1,17 @@
+"""
+Veniamin Knyazev
+CS 5001 Spring 2022
+Final Project- Marble Class
+"""
 from Marble import Marble
 from Point import Point
-def main():
 
+def main():
 
     marble = Marble(Point(-350,350))
     s = marble.new_screen()
     marble.draw_play_board()
     marble.write_score()
-
-
     s.onclick(marble.click_circles)
     s.mainloop()
 
